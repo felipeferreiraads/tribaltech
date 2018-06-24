c7b62811ae4e196203c2d48a9f050fb1737072e2
Felipe Ferreira <felipe.ferreira.ads@gmail.com>
2018-06-24 20:57:17 -0300

    orimizações e video banner

---
+++ source/js/components/video.js
@@ -0,0 +1,8 @@
+$('.video').vide({
+  mp4: 'assets/banner.mp4',
+  webm: 'assets/banner.webm',
+  poster: 'assets/img/banner.jpg'
+}, {
+    position: '49% 50%',
+    posterType: 'jpg'
+});
--- source/js/index.js
+++ source/js/index.js
@@ -2,8 +2,11 @@
  * Components.
  */
 
+import 'vide'
+
 import './components/countdown'
 import './components/maps'
 import './components/artist'
 import './components/scroll'
 import './components/menu'
+import './components/video'
